0757b21a0e69ca540b89aed210fb91c7c177899a
Zaid Frazao <zaidfrazao@gmail.com>
2016-12-18 15:16:52 +0200

    Initial commit.
    Set up the workspace for the development of the Sportomatic web
    application. Use Node.js and npm for package management. Use
    Webpack for build automation. Use React.js as the dev framework.
    Use Material UI for pre-built Material Design components. Use
    Babel and related plugins for JSX, ES6 and ES7 transpiling. Create
    basic file structure.

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname + "/app",
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./index.js",
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015', 'stage-0'],
+        plugins: [
+          'react-html-attrs', 
+          'transform-class-properties', 
+          'transform-decorators-legacy'
+        ]
+      }
+    }]
+  },
+  output: {
+    path: __dirname + "/build",
+    filename: "index.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ]
+};
